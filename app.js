ea01f2f6f442cb5fbd45f37cf3a255273b15fdc5
AnshikaAgrawal5501 <anshikaagrawal5501@gmail.com>
2021-05-28 00:35:47 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+const port = 3000;
+
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+let items = ["Buy Food", "Cook Food", "Eat Food"];
+
+app.get('/', function(req, res) {
+
+    const today = new Date();
+
+
+    options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+    const day = today.toLocaleDateString("en-US", options);
+
+    res.render('list', { kindOfDay: day, newItems: items });
+});
+
+app.post('/', function(req, res) {
+    item = req.body.newItem;
+    items.push(item);
+
+    res.redirect("/");
+});
+
+app.listen(process.env.PORT || port, function() {
+    console.log(`Example app listening at http://localhost:${port}`);
+});
