540a7e6fe62a3c0aecbb642fa9e41cfcc54183ea
AnshikaAgrawal5501 <anshikaagrawal5501@gmail.com>
2021-05-28 20:16:01 +0530

    added the delete item feature

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const date = require(__dirname + '/date.js');
 
 const app = express();
 const port = 3000;
@@ -7,18 +8,13 @@ const port = 3000;
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
+app.use(express.json({ limit: '1mb' }));
 
 let items = [];
 
 app.get('/', function(req, res) {
 
-    const today = new Date();
-
-    // options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
-    // const day = today.toLocaleDateString("en-US", options);
-
-    const day = today;
-
+    const day = date();
     res.render('list', { kindOfDay: day, newItems: items });
 });
 
@@ -26,11 +22,15 @@ app.post('/', function(req, res) {
     item = req.body.newItem;
     items.push(item);
 
-    res.redirect("/");
+    res.redirect('/');
 });
 
-app.post('/delete', function(req, res) {
-    console.log(req);
+app.get('/deleteItem/:id', function(req, res) {
+
+    const ind = req.params.id;
+    items.splice(ind, 1);
+
+    res.redirect('/');
 });
 
 app.listen(process.env.PORT || port, function() {
